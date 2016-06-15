e0460c0f33eee8daad75f884dc00b98850783532
Pajace <Pajace@gmail.com>
2016-06-15 15:47:01 +0800

    Implement list data for mongo db

--- src/main/scala/com/logdown/mycodetub/db/MongoDb.scala
+++ src/main/scala/com/logdown/mycodetub/db/MongoDb.scala
@@ -2,6 +2,7 @@ package com.logdown.mycodetub.db
 
 import java.util.concurrent.TimeUnit
 
+import com.google.gson.Gson
 import com.mongodb.async.client.Observable
 import org.mongodb.scala.bson.collection.immutable.Document
 import org.mongodb.scala.model.Filters
@@ -9,7 +10,6 @@ import org.mongodb.scala.{Completed, MongoClient}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-
 import com.mongodb.client.model.UpdateOptions
 import org.mongodb.scala._
 import org.mongodb.scala.bson.BsonString
@@ -21,10 +21,10 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 
-object DBConnector {
-    val mongoClient = MongoClient("mongodb://10.8.33.30:27017/")
+object MongoDbConnector {
+    private val mongoClient = MongoClient("mongodb://10.8.33.30:27017/")
 
-    val database = mongoClient.getDatabase("bookstore")
+    private val database = mongoClient.getDatabase("bookstore")
 
     def fetchCollection(collectionName: String) = database.getCollection(collectionName)
 }
@@ -32,13 +32,10 @@ object DBConnector {
 /**
   * Created by pajace_chen on 2016/6/13.
   */
-class MongoDb extends Database[Book] {
-
-    val CollectionName = "books"
+class MongoDb(collection: MongoCollection[Document] = MongoDbConnector.fetchCollection("books")) extends Database[Book] {
 
     override def addData(isbn: String, bookJsonString: String): String = {
         try {
-            val collection = DBConnector.fetchCollection(CollectionName)
             val bookDocument = Document.apply(bookJsonString)
             val insertFuture = collection.insertOne(bookDocument).toFuture()
 
@@ -52,18 +49,22 @@ class MongoDb extends Database[Book] {
     override def deleteDataByKey(isbn: String): Observable[Completed] = ???
 
     override def updateData(isbn: String, value: String): String = {
-        val document :Document = Document.apply(value)
+        val document: Document = Document.apply(value)
 
-        val collection = DBConnector.fetchCollection(CollectionName)
         val update = collection.replaceOne(Filters.eq("isbn", isbn), document)
 
         Await.result(update.head(), Duration(10, TimeUnit.SECONDS)).toString
     }
 
-    override def listData(): List[Book] = ???
+    override def listData(): List[Book] = {
+        val findAll: Future[Seq[Document]] = collection.find().projection(excludeId()).toFuture()
+        val gson: Gson = new Gson
+        val allData = Await.result(findAll, Duration(20, TimeUnit.SECONDS)).map(f => gson.fromJson(f.toJson(), classOf[Book]))
+        allData.toList
+    }
 
     override def getDataByKey(isbn: String): String = {
-        val collection = DBConnector.fetchCollection(CollectionName)
+
         val findFuture = collection.find(Filters.eq("isbn", isbn))
             .projection(fields(excludeId()))
 
--- src/test/scala/com/logdown/mycodetub/MongoDbTest.scala
+++ src/test/scala/com/logdown/mycodetub/MongoDbTest.scala
@@ -1,30 +1,139 @@
 package com.logdown.mycodetub
 
+import java.util.concurrent.TimeUnit
+
 import com.github.simplyscala.{MongoEmbedDatabase, MongodProps}
-import com.logdown.mycodetub.db.{Book, Database, MongoDb}
-import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import com.google.gson.Gson
+import com.logdown.mycodetub.db.{Book, Database, MongoDb, MongoDbConnector}
+import org.mongodb.scala.Completed
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterEach, FlatSpec, Matchers}
 import spray.json._
+
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.duration.Duration
 
 /**
   * Created by pajace_chen on 2016/6/14.
   */
-class MongoDbTest extends FlatSpec with Matchers with MongoEmbedDatabase with BeforeAndAfter {
+class MongoDbTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
-    var mongoInstance: MongodProps = null
+    val testCollection = MongoDbConnector.fetchCollection("test")
+    val mongoDb: Database[Book] = new MongoDb(testCollection)
 
-    override protected def before(fun: => Any): Unit = {
-        try {
-            mongoInstance = mongoStart(27017)
-        } catch {
-            case ex: Exception =>
-        }
+    override protected def beforeEach(): Unit = {
+        super.beforeEach()
+        val dropFuture = testCollection.drop().toFuture()
+        Await.result(dropFuture, Duration(10, TimeUnit.SECONDS))
     }
 
-    override protected def after(fun: => Any): Unit = {
-        mongoStop(mongoInstance)
+    override protected def afterEach(): Unit = {
+        super.beforeEach()
+        val dropFuture = testCollection.drop().toFuture()
+        Await.result(dropFuture, Duration(10, TimeUnit.SECONDS))
     }
 
-    val mongoDb: Database[Book] = new MongoDb
+    val booksData = List(
+        """
+          |{
+          |"isbn":"9789863476733",
+          |"name":"Agile學習手冊 : Scrum、XP、精實和看板方法",
+          |"author":"史泰馬恩 ; 葛林 ; 陳佳新",
+          |"publishing":"碁峰資訊",
+          |"version":"初版",
+          |"price":680.0
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789862168219 ",
+          |"name":"脈絡思考創新 = : 喚醒設計思維的3個原點",
+          |"author":"蕭瑞麟",
+          |"publishing":"天下遠見",
+          |"version":"第一版",
+          |"price":350
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789862729717",
+          |"name":"菁英力 : 職場素養進階課 = Professionalism",
+          |"author":"陳嫦芬",
+          |"publishing":"商周出版",
+          |"version":"初版",
+          |"price":420
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789863208112",
+          |"name":"7個習慣決定未來 : 柯維給年輕人的成長藍圖",
+          |"author":"柯維",
+          |"publishing":"遠見天下文化出版",
+          |"version":"第一版",
+          |"price":380
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789862728956",
+          |"name":"像工程師一樣思考",
+          |"author":"馬德哈文 ; 陳雅莉",
+          |"publishing":"商周出版",
+          |"version":"初版",
+          |"price":300
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789866031793",
+          |"name":"系統思考 : 克服盲點、面對複雜性、見樹又見林的整體思考",
+          |"author":"麥道斯 ; 邱昭良",
+          |"publishing":"經濟新潮社出版",
+          |"version":"初版",
+          |"price":450
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789863475385",
+          |"name":"JavaScript應用程式開發實務",
+          |"author":"艾里亞特 ; 楊仁和",
+          |"publishing":"碁峰資訊",
+          |"version":"初版",
+          |"price":480
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789864340408",
+          |"name":"JavaScript設計模式與開發實踐",
+          |"author":"曾探",
+          |"publishing":"博碩文化",
+          |"version":"初版",
+          |"price":460
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9789863791621",
+          |"name":"奠定大數據的基石 : NoSQL資料庫技術",
+          |"author":"皮雄軍",
+          |"publishing":"佳魁資訊",
+          |"version":"初版",
+          |"price":560
+          |}
+        """.stripMargin,
+        """
+          |{
+          |"isbn":"9862763833",
+          |"name":"團隊之美",
+          |"author":"史泰馬恩 ; 葛林 ; 鄭明輝",
+          |"publishing":"	碁峰資訊",
+          |"version":"初版",
+          |"price":580
+          |}
+        """.stripMargin
+    )
 
 
     "addData" should "add data into mongo db" in {
@@ -61,10 +170,23 @@ class MongoDbTest extends FlatSpec with Matchers with MongoEmbedDatabase with Be
                |}
             """.stripMargin
         mongoDb.addData(bookIsbn, bookJson)
-        mongoDb.getDataByKey(bookIsbn).parseJson should be (bookJson.parseJson)
+        mongoDb.getDataByKey(bookIsbn).parseJson should be(bookJson.parseJson)
 
         val expected = bookJson.replace("初版", "再版").replace("560", "888")
         mongoDb.updateData(bookIsbn, expected)
-        mongoDb.getDataByKey(bookIsbn).parseJson should be (expected.parseJson)
+        mongoDb.getDataByKey(bookIsbn).parseJson should be(expected.parseJson)
     }
+
+    "listData" should "return all books list" in {
+        // add 10 books
+        val gson: Gson = new Gson
+        booksData.foreach(b => mongoDb.addData("", b))
+        val expectedBookList: List[Book] = booksData.map((b: String) => gson.fromJson(b, classOf[Book]))
+
+        val actualBookList: List[Book] = mongoDb.listData()
+
+        actualBookList.size should be(10)
+        actualBookList.foreach((b: Book) => expectedBookList should contain(b))
+    }
+
 }
