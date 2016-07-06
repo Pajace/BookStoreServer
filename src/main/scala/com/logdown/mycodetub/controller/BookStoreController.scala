a6828cfb15aef89083529d18f3e9e5eb3772b635
Pajace Y.J. Chen <pajace@gmail.com>
2016-07-06 10:21:11 +0800

    Add future pool for book store controller

--- src/main/scala/com/logdown/mycodetub/controller/BookStoreController.scala
+++ src/main/scala/com/logdown/mycodetub/controller/BookStoreController.scala
@@ -5,6 +5,7 @@ import com.logdown.mycodetub.data.Book
 import com.logdown.mycodetub.db.{DbOperation, MongodbOperation}
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.utils.FuturePools
 
 /**
   * Created by pajace_chen on 2016/6/6.
@@ -31,67 +32,89 @@ object BookStoreApi {
 @Singleton
 class BookStoreController @Inject()(db: MongodbOperation) extends Controller {
 
+
+    //see http://twitter.github.io/finatra/user-guide/build-new-http-server/controller.html#requests
+    private val futurePool = FuturePools.unboundedPool("CallbackConverter")
+
     get(BookStoreApi.path_get(":isbn")) {
         request: Request =>
-            db.findByIsbn(request.params("isbn")) match {
-                case Some(b) => b
-                case None => response.notFound
+            futurePool {
+                db.findByIsbn(request.params("isbn")) match {
+                    case Some(b) => b
+                    case None => response.notFound
+                }
             }
     }
 
     get(BookStoreApi.path_list) {
         request: Request =>
-            response.ok
-            db.listAll()
+            futurePool {
+                response.ok
+                db.listAll()
+            }
+
     }
 
     get(BookStoreApi.path_find_by_name) {
         request: Request =>
-            val findName = request.params.getOrElse("name", "")
-            val result = db.findByName(findName)
-            result
+            futurePool {
+                val findName = request.params.getOrElse("name", "")
+                val result = db.findByName(findName)
+                result
+            }
     }
 
     get(BookStoreApi.path_find_by_include_name) {
         request: Request =>
-            val includeName = request.params.getOrElse("name", "")
-            info("query with include name: " + includeName)
-            val result = db.findByIncludeName(includeName)
-            result
+            futurePool {
+                val includeName = request.params.getOrElse("name", "")
+                info("query with include name: " + includeName)
+                val result = db.findByIncludeName(includeName)
+                result
+            }
     }
 
     post(BookStoreApi.path_create) {
         book: Book =>
-            val result = db.insertBook(book)
-            if (result.isRight) response.created.location(s"/bookstore/${book.isbn}").body(DbOperation.ResultSuccess)
-            else response.created.location(s"/bookstore/${book.isbn}").body(DbOperation.ResultFailed)
+            futurePool {
+                val result = db.insertBook(book)
+                if (result.isRight) response.created.location(s"/bookstore/${book.isbn}").body(DbOperation.ResultSuccess)
+                else response.created.location(s"/bookstore/${book.isbn}").body(DbOperation.ResultFailed)
+
+            }
     }
 
     post(BookStoreApi.path_add_many) {
         books: List[Book] =>
-            val result = db.insertManyBooks(books)
-            val resultLocationList = books.map((b: Book) => BookStoreApi.path_get(b.isbn))
-            if (result.isRight)
-                response.created.body(resultLocationList)
-            else
-                response.serviceUnavailable.body("batch add failed")
+            futurePool {
+                val result = db.insertManyBooks(books)
+                val resultLocationList = books.map((b: Book) => BookStoreApi.path_get(b.isbn))
+                if (result.isRight)
+                    response.created.body(resultLocationList)
+                else
+                    response.serviceUnavailable.body("batch add failed")
+            }
     }
 
     put(BookStoreApi.path_update) {
         book: Book =>
-            val updateResult = db.updateBook(book)
-            if (updateResult.isRight)
-                response.accepted.location(s"/bookstore/${book.isbn}")
-            else
-                response.notFound.body(s"${book.isbn} not found")
+            futurePool {
+                val updateResult = db.updateBook(book)
+                if (updateResult.isRight)
+                    response.accepted.location(s"/bookstore/${book.isbn}")
+                else
+                    response.notFound.body(s"${book.isbn} not found")
+            }
     }
 
     delete(BookStoreApi.path_delete(":isbn")) {
         request: Request =>
-            val key = request.params("isbn")
+            futurePool {
+                val key = request.params("isbn")
 
-            if (db.deleteBook(key).isRight) response.accepted
-            else response.notFound
+                if (db.deleteBook(key).isRight) response.accepted
+                else response.notFound
+            }
     }
 
 }
